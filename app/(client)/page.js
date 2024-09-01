4dadd897efaa5738935dcb1f9c6e5525db1cedaf
pro-minhaj <parsonal322532@gmail.com>
2024-09-01 15:18:14 +0600

    Remove Animated Container

--- app/(client)/page.js
+++ app/(client)/page.js
@@ -1,13 +1,9 @@
-import BestProjectSection from './components/BestProjectSection/BestProjectSection';
 import HeroSection from './components/HeroSection/HeroSection';
-import SkillsSection from './components/SkillsSection/SkillsSection';
 
 const HomePage = () => {
     return (
         <>
             <HeroSection />
-            <BestProjectSection />
-            {/* <SkillsSection /> */}
         </>
     );
 };
