81202e67bf8f44325ef663f025f0a1e933e847c4
Md Minhaj <139013851+prominhaj@users.noreply.github.com>
2025-04-15 15:54:51 +0600

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang='en'>
           <head>
-        <meta name="cryptomus" content="e78d72a9" />
+       <meta name="cryptomus" content="245c20f0" />
       </head>
             <body className={cn(inter.className, 'dark:bg-[#03060D] bg-[#FFFFFF]')}>
                 <ThemeProvider
